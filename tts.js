2f3a466522b63dc4290cb28189b4b5406b9d34c1
mr4k <buzzik13@gmail.com>
2021-03-26 14:17:58 +0200

    fixed non-wav files

--- tts.js
+++ tts.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const stream = require('stream');
 const getDirFiles = require('./src/get-dir-files.js');
 const { promisify } = require('util');
 const readFile = promisify(fs.readFile);
@@ -7,6 +8,8 @@ const { getCreds } = require('json-credentials');
 const TextToSpeechV1 = require('ibm-watson/text-to-speech/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
 const pressToExit = require('./src/press-to-exit.js');
+const pipeline = promisify(stream.pipeline);
+const got = require('got');
 const configFile = 'config.json';
 let iterator = 0;
 let ttsAPI, credentials, txtArr;
@@ -46,11 +49,17 @@ let ttsAPI, credentials, txtArr;
       try {
         const result = await ttsAPI.synthesize(params);
         let audio = result.result;
-        if (config.extension === 'wav') {
+        if (config.extension === 'wav' && config.mode === 'api') {
           audio = await ttsAPI.repairWavHeaderStream(audio);
+          fs.writeFileSync(resultFilePath, audio);
+        } else {
+          let newFile = fs.createWriteStream(resultFilePath);
+          await pipeline(audio, newFile);
+          // audio.pipe(writeableStream);
         }
-        fs.writeFileSync(resultFilePath, audio);
-        console.log(`${resultFilePath} saved.`);
+
+        //
+        // console.log(`${resultFilePath} saved.`);
       } catch (error) {
         console.log(error);
         return;
